f7b43c69fe9a4902cad9fd6f294ab32a2ffa1277
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-21 10:07:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,6 @@
 def oxford_comma(array)
   if array.length == 1
     return array.join
-  elsif array.
-
-  str = array.join(", ")
-  str[str.rindex(",")] = ", and"
-  return str
+  elsif array.length == 2
+    
 end
